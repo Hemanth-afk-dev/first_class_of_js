d5777d4aef270f901d571fba43d85aa8df1ece7c
Hemanth-afk-dev <2400030277@kluniversity.in>
2025-09-08 12:20:51 +0530

    const,let,variable demo

---
+++ script.js
@@ -0,0 +1,24 @@
+let a=10;
+let b=20;
+//let c=b%a;
+let c=a%b;
+document.writeln("output for c is :  ",c)
+
+const d = "Hemanth ";
+const e = "2400030277";
+const f = 30277;
+const g = d+e+f;
+document.writeln("output for g is :  ",g)
+
+const h = 170;
+const i = 4;
+const j = "Hemanth";
+const k = h+i+j;
+document.writeln("output for k is : ",k)
+
+var l = 115;
+var m = 2;
+let n = a**2;
+let o = a*b;
+document.writeln("output for n is ",n)
+document.wiiteln("output for o is : ",o)
